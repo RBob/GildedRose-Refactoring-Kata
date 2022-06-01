04d694a3e8ed0b336d4054f848e5fe985b810854
costiro <Robert.COSTIN@ext.ec.europa.eu>
2022-06-01 10:40:40 +0200

    2nd refactor

--- Java/src/main/java/com/gildedrose/refactored1/LegendaryItem.java
+++ Java/src/main/java/com/gildedrose/refactored1/LegendaryItem.java
@@ -4,11 +4,6 @@ import com.gildedrose.Item;
 
 public class LegendaryItem extends AbstractItem {
 
-    public LegendaryItem(int qualityChangeRate, Item item) {
-        super(qualityChangeRate, item);
-        maxQuality = 80;
-    }
-
     public LegendaryItem(int qualityChangeRate, int maxQuality, Item item) {
         super(qualityChangeRate, item);
         AbstractItem.maxQuality = maxQuality;
