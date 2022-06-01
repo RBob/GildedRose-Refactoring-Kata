a35b97843b6f560ea195a6c33fd6b84f16488178
costiro <Robert.COSTIN@ext.ec.europa.eu>
2022-06-01 10:50:34 +0200

    2nd refactor

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -35,55 +35,7 @@ class GildedRoseTest {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-        // degrading item
-        assertEquals("+5 Dexterity Vest", app.items[0].name);
-        assertEquals(9, app.items[0].sellIn);
-        assertEquals(19, app.items[0].quality);
-
-        // ageing item
-        assertEquals("Aged Brie", app.items[1].name);
-        assertEquals(1, app.items[1].sellIn);
-        assertEquals(1, app.items[1].quality);
-
-        // degrading item
-        assertEquals("Elixir of the Mongoose", app.items[2].name);
-        assertEquals(-2, app.items[2].sellIn);
-        assertEquals(5, app.items[2].quality);
-
-        // legendary item
-        assertEquals("Sulfuras, Hand of Ragnaros", app.items[3].name);
-        assertEquals(0, app.items[3].sellIn);
-        assertEquals(80, app.items[3].quality);
-
-        // legendary item
-        assertEquals("Sulfuras, Hand of Ragnaros", app.items[4].name);
-        assertEquals(-1, app.items[4].sellIn);
-        assertEquals(80, app.items[4].quality);
-
-        // fast ageing item
-        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[5].name);
-        assertEquals(14, app.items[5].sellIn);
-        assertEquals(21, app.items[5].quality);
-
-        // fast ageing item
-        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[6].name);
-        assertEquals(9, app.items[6].sellIn);
-        assertEquals(42, app.items[6].quality);
-
-        // fast ageing item
-        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[7].name);
-        assertEquals(4, app.items[7].sellIn);
-        assertEquals(33, app.items[7].quality);
-
-        // conjured item
-        assertEquals("Conjured Mana Cake", app.items[8].name);
-        assertEquals(2, app.items[8].sellIn);
-        assertEquals(4, app.items[8].quality);
-
-        // conjured item
-        assertEquals("Conjured Mana Cake", app.items[9].name);
-        assertEquals(-2, app.items[9].sellIn);
-        assertEquals(2, app.items[9].quality);
+        assertResults(app);
     }
 
     @Test
@@ -91,6 +43,11 @@ class GildedRoseTest {
 
         GildedRose app = new GildedRose(items);
         app.updateQualityRefactored1();
+
+        assertResults(app);
+    }
+
+    private void assertResults(GildedRose app) {
 
         // degrading item
         assertEquals("+5 Dexterity Vest", app.items[0].name);
